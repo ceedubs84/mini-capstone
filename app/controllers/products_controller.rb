61b2ef0284b21a2ad6f80b3030a651a5a7c1ff0f
Chris Woodside <christopherwoodside@gmail.com>
2016-11-08 19:37:01 -0600

    Added All Product method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def one_product_method
+
     render 'one_product.html.erb'
+  end
+
+  def all_product_method
+    render 'all_product.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/one_product_url' => 'products#one_product_method'
+  get '/all_product_url' => 'products#all_product_method'
 end
