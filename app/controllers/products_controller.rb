886ca68e2f72b309f062568b1ec86726fb146334
Chris Woodside <christopherwoodside@gmail.com>
2016-11-08 20:59:30 -0600

    Added HTML info

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
   def one_product_method
-
+    @product = Product.first
     render 'one_product.html.erb'
   end
 
   def all_product_method
+    @products = Product.all
     render 'all_product.html.erb'
   end
 end
