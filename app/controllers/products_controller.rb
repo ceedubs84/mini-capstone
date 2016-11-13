edac1e97ef5acfbf3c61474019a650d15e5a4287
Chris Woodside <christopherwoodside@gmail.com>
2016-11-13 09:57:15 -0600

    Added new and create routes, methods, veiws

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,29 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image: params["image"],
+      description: params["description"]
+      )
+    product.save
+    render 'create.html.erb'
+  end
+
   def show
     product_id = params[:id]
     @product = Product.find_by(id: product_id)
     render 'show.html.erb'
   end
 
-  def index
-    @products = Product.all
-    render 'index.html.erb'
-  end
+  
 end
