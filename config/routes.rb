41ab587d8248408a7c9cebc391acb54607cc4704
Chris Woodside <christopherwoodside@gmail.com>
2016-11-13 09:03:49 -0600

    Added index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/products' => 'products#index'
-  get '/product/:id' => 'products#show'
+  get '/products/:id' => 'products#show'
 end
