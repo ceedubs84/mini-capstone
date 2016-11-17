c604e9ca0b813ff19f9408aeabf5d50628d7d67d
Chris Woodside <christopherwoodside@gmail.com>
2016-11-16 18:44:01 -0600

    Added Tax and Total model methods to index and show views

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,21 @@ class Product < ApplicationRecord
   def dollar_price
     dollar_price = "$" + price
   end
+
+  def sale_message
+    sale_message = ""
+    if price.to_i < 10
+      sale_message = "Discount Item!"
+    else 
+      sale_message = "Quotidian Worth!"
+    end
+  end
+
+  def tax
+    tax = (price.to_i * 0.09).to_f
+  end
+
+  def total
+    total = "$" + (price.to_f + tax).to_s
+  end
 end
