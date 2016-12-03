a094c493f6a6b2e6aba9fd2a26e37491ef8f0f57
Chris Woodside <christopherwoodside@gmail.com>
2016-12-03 15:55:38 -0600

    Created index action in CartedProducts controller and created index view for CartedProducts.

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,24 @@
 class CartedProductsController < ApplicationController
+  def index
+    @carted_products = current_user.carted_products.where(status: "carted")
+    if @carted_products.length > 0
+      render 'index.html.erb'
+    else
+      flash[:warning] = "You don't have any items in your shopping cart. BUY SOME STUFF NOW!"
+      redirect_to "/"
+    end
+  end
+
+
+  def create
+    carted_product = CartedProduct.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: "carted"
+    )
+    carted_product.save
+    flash[:success] = "Added to cart successfullly!"
+    redirect_to "/carted_products"
+  end
 end
