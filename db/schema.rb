95574f1f73be1a34be085c649766acc672de7afa
Chris Woodside <christopherwoodside@gmail.com>
2016-12-03 15:27:21 -0600

    Removed quantity from Orders table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203212313) do
+ActiveRecord::Schema.define(version: 20161203212623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20161203212313) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "user_id"
-    t.integer  "quantity"
     t.decimal  "subtotal",   precision: 9, scale: 2
     t.decimal  "tax",        precision: 9, scale: 2
     t.datetime "created_at",                         null: false
