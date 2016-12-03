6dd00a1b0e85408c648ff4ab67fef86121c7199c
Chris Woodside <christopherwoodside@gmail.com>
2016-12-03 15:46:46 -0600

    Changed form & button for Add to Cart and changed routes to match.

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,19 @@ Rails.application.routes.draw do
   get '/products/:id' => 'products#show'
   get '/products/:id/edit' => 'products#edit'
   patch '/products/:id' => 'products#update'
-  delete '/products/:id' => 'products#destroy'
+  delete '/products/:id' => 'products#delete'
   post '/search' => 'products#search'
 
   post '/orders' => 'orders#create'
   get '/orders/:id' => 'orders#show'
+
+  get '/carted_products' => 'carted_products#index'
+  post '/carted_products' => 'carted_products#create'
+  delete '/carted_products/:id' => 'carted_products#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
 end
