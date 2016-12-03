20a3af571c80c049522e358952001dd35d2fc9ab
Chris Woodside <christopherwoodside@gmail.com>
2016-12-03 15:59:11 -0600

    Updated Orders controller create action.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,13 @@
 class OrdersController < ApplicationController
   def create
-    product = Product.find_by(id: params[:product_id])
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
-    order = Order.new(
-      user_id: current_user.id,
-      quantity: params[:quantity],
-      product_id: params[:product_id],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total
-    )
+    order = Order.new(user_id: current_user.id)
     order.save
+    
+    carted_products = current_user.carted_products.where(status: "carted")
+    carted_products.update_all(status: "purchased", order_id: order.id)
+
+    order.update_all_totals
+
     flash[:success] = "Your order was created successfully!"
     redirect_to "/orders/#{order.id}"
   end
